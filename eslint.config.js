12dee703e5e422a32720ea99b228520dba41c7ed
Michael Schwobe <mschwobe@gmail.com>
2025-03-01 13:53:42 -0600

    Updated TS and ESLint configs

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,8 @@
 // @ts-check
 import { includeIgnoreFile } from "@eslint/compat";
-import eslint from "@eslint/js";
+import eslintJs from "@eslint/js";
+import pluginJson from "@eslint/json";
+import pluginMarkdown from "@eslint/markdown";
 import pluginJsxA11y from "eslint-plugin-jsx-a11y";
 import pluginReact from "eslint-plugin-react";
 import pluginReactHooks from "eslint-plugin-react-hooks";
@@ -14,17 +16,51 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const gitignorePath = path.resolve(__dirname, ".gitignore");
 
-export default tseslint.config(
-  // Exclude gitignore paths
+/** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
+const config = [
+  // Exclusions
   includeIgnoreFile(gitignorePath),
 
+  // Markdown
+  {
+    ...pluginMarkdown.configs.recommended[0],
+    files: ["**/*.md"],
+    language: "markdown/gfm",
+  },
+
+  // JSON
+  {
+    ...pluginJson.configs.recommended,
+    files: ["**/*.json"],
+    ignores: ["tsconfig.*.json"],
+    language: "json/json",
+  },
+
+  // JSONC
+  {
+    ...pluginJson.configs.recommended,
+    files: ["**/*.jsonc", ".vscode/*.json", "tsconfig.json", "tsconfig.*.json"],
+    language: "json/jsonc",
+  },
+
   // JavaScript
-  eslint.configs.recommended,
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+    rules: eslintJs.configs.recommended.rules,
+  },
 
   // TypeScript
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
+  ...[
+    ...tseslint.configs.recommended,
+    ...tseslint.configs.stylisticTypeChecked,
+  ].map((conf) => ({
+    ...conf,
+    files: ["**/*.ts", "**/*.tsx"],
+  })),
+
+  // JavaScript & TypeScript
   {
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
     languageOptions: {
       globals: { ...globals.browser, ...globals.node },
       parserOptions: {
@@ -34,28 +70,36 @@ export default tseslint.config(
     },
   },
 
-  // Disable TypeScript rules for JavaScript files
+  // React
   {
-    files: ["**/*.js"],
-    extends: [tseslint.configs.disableTypeChecked],
+    ...pluginReact.configs.flat.recommended,
+    files: ["**/*.ts", "**/*.tsx"],
+    settings: { react: { version: "detect" } },
   },
 
-  // React
-  { settings: { react: { version: "detect" } } },
-  pluginReact.configs.flat.recommended,
-  pluginReact.configs.flat["jsx-runtime"],
+  // React JSX Runtime
+  {
+    ...pluginReact.configs.flat["jsx-runtime"],
+    files: ["**/*.ts", "**/*.tsx"],
+  },
 
   // React Hooks
   {
-    // @ts-expect-error - works but lib types incorrect
-    plugins: { "react-hooks": pluginReactHooks },
-    // @ts-expect-error - works but lib types incorrect
-    rules: pluginReactHooks.configs.recommended.rules,
+    ...pluginReactHooks.configs["recommended-latest"],
+    files: ["**/*.ts", "**/*.tsx"],
   },
 
   // React Refresh
-  pluginReactRefresh.configs.vite,
+  {
+    ...pluginReactRefresh.configs.vite,
+    files: ["**/*.ts", "**/*.tsx"],
+  },
 
   // JSX A11y
-  pluginJsxA11y.flatConfigs.recommended,
-);
+  {
+    ...pluginJsxA11y.flatConfigs.recommended,
+    files: ["**/*.js", "**/*.ts", "**/*.tsx"],
+  },
+];
+
+export default config;
