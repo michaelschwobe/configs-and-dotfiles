657e9a96002a6919f9645042b0ad854088d453c7
Michael Schwobe <mschwobe@gmail.com>
2025-05-10 14:43:43 -0500

    Updated to a simpler vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import tsConfigPaths from "vite-tsconfig-paths";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 /**
  * @see https://vite.dev/config/
  */
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    tsConfigPaths({
-      projects: ["./tsconfig.json"],
-    }),
-  ],
+  plugins: [tailwindcss(), react(), tsconfigPaths()],
 });
