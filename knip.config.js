b8d55f157de2b76231f40de151bc0cdf494e0445
Michael Schwobe <mschwobe@gmail.com>
2025-05-24 09:52:47 -0500

    Updated (dev)dependencies

--- knip.config.js
+++ knip.config.js
@@ -6,11 +6,8 @@
  */
 const config = {
   ignoreDependencies: ["tailwindcss"],
-  entry: ["src/{index,cli,main}.{js,cjs,mjs,jsx,ts,cts,mts,tsx}", "reset.d.ts"],
-  project: [
-    "**/*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}",
-    "**/.*.{js,cjs,mjs,jsx,ts,cts,mts,tsx}",
-  ],
+  entry: ["src/{index,main}.{ts,tsx}", "reset.d.ts"],
+  project: ["**/*.{js,ts,tsx}", "**/.*.{js,ts,tsx}"],
 };
 
 export default config;
